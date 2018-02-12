4e5b08dd071ba713e8e821b77acc507ac0c69632
Narek Ghevandiani <n.ghevandiani@gmail.com>
2018-02-12 14:24:11 +0400

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['babel-polyfill', './src/app.js']
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'extension/lib/'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }    
+    ]
+  }  
+};
