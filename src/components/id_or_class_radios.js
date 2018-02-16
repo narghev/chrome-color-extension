79355f2473e337ac68daa2e0324efdbe125acee7
Narek Ghevandiani <n.ghevandiani@gmail.com>
2018-02-16 14:06:19 +0400

    radio buttons styling and functionallity fix

--- src/components/id_or_class_radios.js
+++ src/components/id_or_class_radios.js
@@ -8,32 +8,40 @@ const IdOrClassRadios = ({element, idOrClass, onChange}) => {
 
   return (
     <div className="extension-color-modifier-id-or-class-radios-wrapper">
-      <label 
-        htmlFor="extension-color-modifier-id-or-class-radios-class"
-        className={`extension-color-modifier-id-or-class-radios-label ${!elementId && 'disabled'}`}
-      >Class</label>
-      <input
-        id="extension-color-modifier-id-or-class-radios-class"
-        type="radio"
-        name="extension-color-modifier-id-or-class-radios"
-        disabled={!elementClass}
-        checked={idOrClass === 'class'}
-        onChange={onChange}
-        value="class"
-      />
-      <label 
-        htmlFor="extension-color-modifier-id-or-class-radios-id"
-        className={`extension-color-modifier-id-or-class-radios-label ${!elementClass && 'disabled'}`}
-      >Id</label>
-      <input
-        id="extension-color-modifier-id-or-class-radios-id"
-        type="radio"
-        name="extension-color-modifier-id-or-class-radios"
-        disabled={!elementId}
-        checked={idOrClass === 'id'}
-        onChange={onChange}
-        value="id"
-      />
+      <div>
+        <label
+          htmlFor="extension-color-modifier-id-or-class-radios-class"
+          className={`extension-color-modifier-id-or-class-radios-label ${!elementId && 'disabled'}`}
+        >
+          Modify this particular element
+        </label>
+        <input
+          id="extension-color-modifier-id-or-class-radios-class"
+          type="radio"
+          name="extension-color-modifier-id-or-class-radios"
+          disabled={!elementId}
+          checked={idOrClass === 'id'}
+          onChange={onChange}
+          value="id"
+        />
+      </div>
+      <div>
+        <label 
+          htmlFor="extension-color-modifier-id-or-class-radios-id"
+          className={`extension-color-modifier-id-or-class-radios-label ${!elementClass && 'disabled'}`}
+        >
+          Modify this and similar elements
+        </label>
+        <input
+          id="extension-color-modifier-id-or-class-radios-id"
+          type="radio"
+          name="extension-color-modifier-id-or-class-radios"
+          disabled={!elementClass}
+          checked={idOrClass === 'class'}
+          onChange={onChange}
+          value="class"
+        />
+      </div>
     </div>
   );
 };
